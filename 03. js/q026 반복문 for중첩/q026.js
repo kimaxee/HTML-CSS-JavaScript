de1d48de61d9176db76c1df51116cea915446ef2
kimaxee <95838445+kimaxee@users.noreply.github.com>
2021-12-16 15:14:00 +0900

    Add files via upload

--- 03. js/q026 반복문 for중첩/q026.js
+++ 03. js/q026 반복문 for중첩/q026.js
@@ -1,6 +1,18 @@
-function dw(dog) {
-    document.write(dog);
+function popup(age, type, name) {
+
+    catAge = age;
+    catType = type;
+    catName = name;
+
+
+    alert("우리집 고양이 나이는 " + catAge +
+        "살입니다. \n품종은 " + catType +
+        "입니다. \n이름은 " + catName + "입니다.");
+
 }
 
+popup(100, "잡종", "야옹이");
 
-dw("asd");
+popup(10, "페르시안", "나비");
+
+popup(37, "스코티시", "호박");
