9d5ed65998fd556a312844da8fd654355c1f575c
kimaxee <95838445+kimaxee@users.noreply.github.com>
2021-12-17 17:38:16 +0900

    Add files via upload

--- 03. js/q030 가위바위보ver2/common.js
+++ 03. js/q030 가위바위보ver2/common.js
@@ -5,16 +5,3 @@ function dw(str) {
 function br() {
     document.write("<br>");
 }
-
-function Scissors() {
-    document.write("<img id=Scissors src='가위.png'>")
-    }
-
-function Rock() {
-    document.write("<img id=Rock src='바위.png'>")
-    }
-
-function Paper() {
-    document.write("<img id=Paper src='보.png'>")
-    }
-var computer = Math.floor(Math.random() * 3) + 1
--- 03. js/q030 가위바위보ver2/q030.js
+++ 03. js/q030 가위바위보ver2/q030.js
@@ -14,53 +14,53 @@ while (true) {
 
       case "가위":
         if (3 == computer) {
-          alert("무승부 \n컴퓨터 : 보 \n플레이어 : 가위");
+          alert("'무승부' \n컴퓨터 : 보 \n플레이어 : 가위");
         }
 
         else if (2 <= computer) {
-          alert("패배 \n컴퓨터 : 바위 \n플레이어 : 가위");
+          alert("'패배' \n컴퓨터 : 바위 \n플레이어 : 가위");
         }
 
         else {
-          alert("승리 \n컴퓨터 : 가위 \n플레이어 : 가위");
+          alert("'승리' \n컴퓨터 : 가위 \n플레이어 : 가위");
         }
         break;
 
       case "바위":
         if (3 == computer) {
-          alert("무승부 \n컴퓨터 : 가위 \n플레이어 : 바위");
+          alert("'무승부' \n컴퓨터 : 가위 \n플레이어 : 바위");
         }
 
         else if (2 <= computer) {
-          alert("패배 \n컴퓨터 : 보 \n플레이어 : 바위");
+          alert("'패배' \n컴퓨터 : 보 \n플레이어 : 바위");
         }
 
         else {
-          alert("승리 \n컴퓨터 : 바위 \n플레이어 : 바위");
+          alert("'승리' \n컴퓨터 : 바위 \n플레이어 : 바위");
         }
         break;
 
       case "보":
         if (3 == computer) {
-          alert("무승부 \n컴퓨터 : 바위 \n플레이어 : 보");
+          alert("'무승부' \n컴퓨터 : 바위 \n플레이어 : 보");
         }
 
         else if (2 <= computer) {
-          alert("패배 \n컴퓨터 : 가위 \n플레이어 : 보");
+          alert("'패배' \n컴퓨터 : 가위 \n플레이어 : 보");
         }
 
         else {
-          alert("승리 \n컴퓨터 : 보 \n플레이어 : 보");
+          alert("'승리' \n컴퓨터 : 보 \n플레이어 : 보");
         }
         break;
     }
   }
   else if (player == "나가기") {
-    alert("수고하셨습니다");
+    alert("'수고하셨습니다'");
     break;
   }
   else {
-    alert("잘못 입력하셨습니다");
+    alert("'잘못 입력하셨습니다'");
   }
 
 }
