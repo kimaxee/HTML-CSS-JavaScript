ce800c6eedd5b835b06f5b494cc75d9f113e6093
kimaxee <95838445+kimaxee@users.noreply.github.com>
2021-12-17 11:52:50 +0900

    Update q029.js

--- 03. js/q029 가위바위보/q029.js
+++ 03. js/q029 가위바위보/q029.js
@@ -1,64 +1,60 @@
 var player = "";
-while (true) {
-    player = prompt("가위, 바위, 보를 입력하세요")
-    if (player == "가위" || player == "바위" || player == "보") {
-        break;
-    }
-    else {
-        alert("잘못 입력했습니다");
-    }
-}
 var computer = Math.floor(Math.random() * 3) + 1
 
-switch (player) {
-    case "가위":
+labelA:
+while (true) {
+  player = prompt("가위, 바위, 보를 입력하세요" + "\n종료 : 나가기")
+  if (player == "가위" || player == "바위" || player == "보") {
+    switch (player) {
+      case "가위":
         if (3 == computer) {
-            alert("컴퓨터 : 가위 \n플레이어 : 무승부")
-            Scissors();
+          alert("컴퓨터 : 가위 \n플레이어 : 무승부")
         }
-        
+
         else if (2 <= computer) {
-            alert("컴퓨터 : 바위 \n플레이어 : 패배");
-            Rock();
+          alert("컴퓨터 : 바위 \n플레이어 : 패배");
         }
-        
+
         else {
-            alert("컴퓨터 : 보 \n플레이어 : 승리");
-            Paper();
+          alert("컴퓨터 : 보 \n플레이어 : 승리");
         }
         break;
 
-    case "바위":
+
+      case "바위":
         if (3 == computer) {
-            alert("컴퓨터 : 바위 \n플레이어 : 무승부")
-            Rock();
+          alert("컴퓨터 : 바위 \n플레이어 : 무승부")
         }
-        
+
         else if (2 <= computer) {
-            alert("컴퓨터 : 보 \n플레이어 : 패배");
-            Paper();
+          alert("컴퓨터 : 보 \n플레이어 : 패배");
         }
-        
+
         else {
-            alert("컴퓨터 : 가위 \n플레이어 : 승리");
-            Scissors();
+          alert("컴퓨터 : 가위 \n플레이어 : 승리");
         }
         break;
 
-    case "보":
+      case "보":
         if (3 == computer) {
-            alert("컴퓨터 : 보 \n플레이어 : 무승부")
-            Paper();
+          alert("컴퓨터 : 보 \n플레이어 : 무승부")
         }
-        
+
         else if (2 <= computer) {
-            alert("컴퓨터 : 가위 \n플레이어 : 패배");
-            Scissors();
+          alert("컴퓨터 : 가위 \n플레이어 : 패배");
         }
-        
+
         else {
-            alert("컴퓨터 : 바위 \n플레이어 : 승리");
-            Rock();
+          alert("컴퓨터 : 바위 \n플레이어 : 승리");
         }
         break;
+    }
+  }
+  else if (player == "나가기") {
+    dw("수고하셨습니다");
+    break;
+  }
+  else {
+    alert("잘못 입력했습니다");
+  }
 }
