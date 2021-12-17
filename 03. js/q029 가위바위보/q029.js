14ceeab51feffb0521618016cd3715bf80da8ccc
kimaxee <95838445+kimaxee@users.noreply.github.com>
2021-12-17 14:35:11 +0900

    Update q029.js

--- 03. js/q029 가위바위보/q029.js
+++ 03. js/q029 가위바위보/q029.js
@@ -1,62 +1,57 @@
-var player = "";
-var computer = 0;
+var player = prompt("가위, 바위, 보를 입력하세요")
+var computer = Math.floor(Math.random() * 3) + 1
 
-
-while (true) {
-  player = prompt("가위, 바위, 보를 입력하세요" + "\n종료 : 나가기")
-  computer = Math.floor(Math.random() * 3) + 1
-  if (player == "가위" || player == "바위" || player == "보") {
-    switch (player) {
-      case "가위":
-        if (3 == computer) {
-          alert("컴퓨터 : 가위 \n플레이어 : 무승부")
-        }
-
-        else if (2 <= computer) {
-          alert("컴퓨터 : 바위 \n플레이어 : 패배");
-        }
-
-        else {
-          alert("컴퓨터 : 보 \n플레이어 : 승리");
-        }
-        break;
-
-      case "바위":
-        if (3 == computer) {
-          alert("컴퓨터 : 바위 \n플레이어 : 무승부")
-        }
-
-        else if (2 <= computer) {
-          alert("컴퓨터 : 보 \n플레이어 : 패배");
-        }
-
-        else {
-          alert("컴퓨터 : 가위 \n플레이어 : 승리");
-        }
-        break;
-
-      case "보":
-        if (3 == computer) {
-          alert("컴퓨터 : 보 \n플레이어 : 무승부")
-        }
-
-        else if (2 <= computer) {
-          alert("컴퓨터 : 가위 \n플레이어 : 패배");
-        }
-
-        else {
-          alert("컴퓨터 : 바위 \n플레이어 : 승리");
-        }
-        break;
+switch (player) {
+  case "가위":
+    if (4 > computer) {
+      alert("컴퓨터 : 보 \n플레이어 : 승리");
+      Paper();
     }
-  }
 
-  else if (player == "나가기") {
-    dw("수고하셨습니다");
+    else if (1 < computer) {
+      alert("컴퓨터 : 바위 \n플레이어 : 패배");
+      Rock();
+    }
+
+    else {
+      alert("컴퓨터 : 가위 \n플레이어 : 무승부")
+      Scissors();
+    }
     break;
-  }
-  else {
-    alert("잘못 입력했습니다");
-  }
-}
 
+  case "바위":
+    if (1 >= computer) {
+      alert("컴퓨터 : 가위 \n플레이어 : 승리");
+      Scissors();
+    }
+
+    else if (1 < computer) {
+      alert("컴퓨터 : 보 \n플레이어 : 패배");
+      Paper();
+    }
+
+    else {
+      alert("컴퓨터 : 바위 \n플레이어 : 무승부")
+      Rock();
+    }
+    break;
+
+  case "보":
+    if (1 >= computer) {
+      alert("컴퓨터 : 바위 \n플레이어 : 승리");
+      Rock();
+    }
+
+    else if (1 < computer) {
+      alert("컴퓨터 : 가위 \n플레이어 : 패배");
+      Scissors();
+    }
+
+    else {
+      alert("컴퓨터 : 보 \n플레이어 : 무승부")
+      Paper();
+    }
+    break;
+  default:
+    alert("잘못 입력하셨습니다");
+}
