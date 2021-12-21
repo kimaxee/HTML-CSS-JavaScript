eb094284667e09549e924524e22e9c3380284c5b
kimaxee <95838445+kimaxee@users.noreply.github.com>
2021-12-21 10:12:30 +0900

    Update q038.js

--- 03. js/q038 로또/q038.js
+++ 03. js/q038 로또/q038.js
@@ -1,34 +1,140 @@
-var myLotto = [2, 6, 10, 26, 34, 37]
-var comLotto = new Array(6);
+var comLotto = new Array();
+var myLotto = [2, 6, 10, 26, 34, 37];
+
+dw("컴 번호 : ");
+for (var i = 0; i < 6; i++) {
+    comLotto[i] = Math.floor(Math.random() * 45 + 1);
+    dw(comLotto[i] + " ");
+}
+
+br();
+
+dw("내 번호 : ");
+for (var j = 0; j < 6; j++) {
+    dw(myLotto[j]+ " ");
+}
 
 var winCount = 0;
 
-
-
-for (var i = 0; i < comLotto.length; i++) { 
-    comLotto[i] = Math.floor(Math.random() * 45 + 1);
+if (myLotto[0] == comLotto[0]) {
+    winCount++;
+}
+else if (myLotto[0] == comLotto[1]) {
+    winCount++;
+}
+else if (myLotto[0] == comLotto[2]) {
+    winCount++;
+}
+else if (myLotto[0] == comLotto[3]) {
+    winCount++;
+}
+else if (myLotto[0] == comLotto[4]) {
+    winCount++;
+}
+else if (myLotto[0] == comLotto[5]) {
+    winCount++;
 }
 
-for (var i = 0; i < comLotto.length; i++) {
-    for (var j = 0; j < myLotto.length; j++) {
-        if (comLotto[i] == myLotto[j]) {
-            winCount++;
-        }
-    }
+
+if (myLotto[1] == comLotto[0]) {
+    winCount++;
+}
+else if (myLotto[1] == comLotto[1]) {
+    winCount++;
+}
+else if (myLotto[1] == comLotto[2]) {
+    winCount++;
+}
+else if (myLotto[1] == comLotto[3]) {
+    winCount++;
+}
+else if (myLotto[1] == comLotto[4]) {
+    winCount++;
+}
+else if (myLotto[1] == comLotto[5]) {
+    winCount++;
 }
 
-dw("컴 번호 : ")
-for (var i = 0; i < comLotto.length; i++) {
-    dw(comLotto[i] + " ")
+
+if (myLotto[2] == comLotto[0]) {
+    winCount++;
+}
+else if (myLotto[2] == comLotto[1]) {
+    winCount++;
+}
+else if (myLotto[2] == comLotto[2]) {
+    winCount++;
+}
+else if (myLotto[2] == comLotto[3]) {
+    winCount++;
+}
+else if (myLotto[2] == comLotto[4]) {
+    winCount++;
+}
+else if (myLotto[2] == comLotto[5]) {
+    winCount++;
+}
+
+
+if (myLotto[3] == comLotto[0]) {
+    winCount++;
+}
+else if (myLotto[3] == comLotto[1]) {
+    winCount++;
+}
+else if (myLotto[3] == comLotto[2]) {
+    winCount++;
+}
+else if (myLotto[3] == comLotto[3]) {
+    winCount++;
+}
+else if (myLotto[3] == comLotto[4]) {
+    winCount++;
+}
+else if (myLotto[3] == comLotto[5]) {
+    winCount++;
+}
+
+
+if (myLotto[4] == comLotto[0]) {
+    winCount++;
+}
+else if (myLotto[4] == comLotto[1]) {
+    winCount++;
+}
+else if (myLotto[4] == comLotto[2]) {
+    winCount++;
+}
+else if (myLotto[4] == comLotto[3]) {
+    winCount++;
+}
+else if (myLotto[4] == comLotto[4]) {
+    winCount++;
+}
+else if (myLotto[4] == comLotto[5]) {
+    winCount++;
+}
+
+
+if (myLotto[5] == comLotto[0]) {
+    winCount++;
+}
+else if (myLotto[5] == comLotto[1]) {
+    winCount++;
+}
+else if (myLotto[5] == comLotto[2]) {
+    winCount++;
+}
+else if (myLotto[5] == comLotto[3]) {
+    winCount++;
+}
+else if (myLotto[5] == comLotto[4]) {
+    winCount++;
+}
+else if (myLotto[5] == comLotto[5]) {
+    winCount++;
 }
 
 br();
-
-dw("내 번호 : ")
-for (var i = 0; i < myLotto.length; i++) {
-    dw(myLotto[i] + " ")
-}
-
-br();
-
 dw(winCount + "개 일치");
+
